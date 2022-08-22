e83f80037879cd24ecae62fb2ac916ee4536efea
Eugene <eugeneayonga@gmail.com>
2022-08-22 15:44:33 +0300

    commit tree c08e9fdc60b512656a8d28d9a2d9f69e206303ae

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr Chocolate", 10 ,"M", "red", "ferocious", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1 ,"F", "orange", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4 ,"M", "black", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7 ,"green", "red", "attentive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2 ,"M", "blue", "alert", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9 ,"F", "indigo", "jolly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8 ,"F", "violet", "fierce", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3 ,"M", "grey", "calm", 1);
